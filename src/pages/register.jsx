b49819515097978c3b8b81dd524c8e8a88a88575
Thunggg <thuannguyen20041028@gmail.com>
2024-10-16 17:48:36 +0700

    bai 90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,32 @@
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, notification } from "antd";
+import {registerUserAPI} from "../services/api.service"
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = Form.useForm();
+    const navigate = useNavigate(); 
 
-    const onFinish = (values) => {
+    const onFinish = async (values) => {
         console.log(">>> check values: ", values)
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.password,
+            values.phone);
+
+        if(res.data){
+            notification.success({
+                message: "Register user",
+                description: "Đăng ký user thành công"
+            })
+
+            navigate("/login");
+        } else{
+            notification.error({
+                message: "Register user",
+                description: "Đăng ký user thất bại"
+            })
+        }
     }
 
     return (
@@ -20,12 +42,12 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Full Name"
                     name="fullName"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your fullName!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
@@ -33,12 +55,12 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Email"
                     name="email"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
@@ -46,12 +68,12 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Password"
                     name="password"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
@@ -59,12 +81,13 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Phone number"
                     name="phone"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }
+                    ]}
                 >
                     <Input />
                 </Form.Item>
@@ -78,6 +101,7 @@ const RegisterPage = () => {
             </div>
         </Form>
     )
+
 }
 
 export default RegisterPage;
