2706c62d0afcde839b8ff5edeb99c2b5ad5727f4
Thunggg <thuannguyen20041028@gmail.com>
2024-10-21 08:18:03 +0700

    bai 112

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,11 @@
 import axios from "axios"
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
+
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL
@@ -6,6 +13,7 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+  NProgress.start();
   if (typeof window !== "undefined" && window
     && window.localStorage
     && window.localStorage.getItem('access_token')) {
@@ -15,12 +23,14 @@ instance.interceptors.request.use(function (config) {
     // Do something before request is sent
     return config;
   }, function (error) {
+    NProgress.done();
     // Do something with request error
     return Promise.reject(error);
   });
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done();
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     if(response.data && response.data.data){
@@ -28,6 +38,7 @@ instance.interceptors.response.use(function (response) {
     }   
     return response;
   }, function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if(error.response && error.response.data) return error.response.data;
